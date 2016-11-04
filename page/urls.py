dffa8e46ad50049f2d261f4ab400438af2c5a2fa
rapospectre <rapospectre@163.com>
2016-11-04 16:17:30 +0800

    <feat>  add guide page
    Author:rapospectre@gmail.com
    
    Desc: add guide page

--- page/urls.py
+++ page/urls.py
@@ -5,4 +5,5 @@ from Dionysus import settings
 
 urlpatterns = patterns('',
                        url(r'^share/(?P<vid>(\d)+)', ShareDetailView.as_view()),
+                       url(r'^guide', GuideView.as_view()),
                        )
--- page/views.py
+++ page/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 # Create your views here.
-from django.views.generic import DetailView
+from django.views.generic import DetailView, TemplateView
 
 from core.models import Share
 
@@ -12,3 +12,7 @@ class ShareDetailView(DetailView):
     model = Share
     pk_url_kwarg = 'vid'
     template_name = 'share.html'
+
+
+class GuideView(TemplateView):
+    template_name = 'guide.html'
