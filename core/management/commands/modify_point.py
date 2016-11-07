4a8ad1f32946e7259f454065f5e7c10df00f335a
rapospectre <rapospectre@163.com>
2016-11-07 15:26:13 +0800

    <feat>  add test script
    Author:rapospectre@gmail.com
    
    Desc: add test script

---
+++ core/management/commands/modify_point.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+from django.core.management.base import BaseCommand
+
+from core.models import AvatarTrack
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        id = args[0]
+        at = AvatarTrack.objects.filter(id=id)
+        if at.exists():
+            at = at[0]
+            data = eval(at.data)
+            for itm in data:
+                dw = itm['size']
+                dh = w * 1.33
+                dw = dw / 2
+                itm['x'] += dw
+                itm['y'] += dh
+            at.data = unicode(data)
+            at.save()
+
