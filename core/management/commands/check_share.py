62ea8050ec0533c27cae57b2195c5fa29179f01a
rapospectre <rapospectre@163.com>
2016-11-10 12:27:54 +0800

    <feat>  add check script
    Author:rapospectre@gmail.com
    
    Desc: add check script

--- core/management/commands/check_share.py
+++ core/management/commands/check_share.py
@@ -2,12 +2,12 @@
 
 from django.core.management.base import BaseCommand
 
-from core.models import AvatarTrack, Video
+from core.models import Share, Video
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for itm in AvatarTrack.objects.all():
+        for itm in Share.objects.all():
             if not itm.source:
                 itm.source = Video.objects.get(url='del')
                 itm.save()
