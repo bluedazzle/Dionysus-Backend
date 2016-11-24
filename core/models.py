15e30fca91d3a2e89fbc6a1dfdcdc4713c066851
rapospectre <rapospectre@163.com>
2016-11-24 15:40:44 +0800

    <fix>  fix when video was deleted the share objs source is null
    Author:rapospectre@gmail.com
    
    Desc: fix when video was deleted the share objs source is null

--- core/models.py
+++ core/models.py
@@ -63,7 +63,7 @@ class MyUser(models.Model):
 
 class Share(BaseModel):
     pid = models.CharField(max_length=256, default='')
-    source = models.ForeignKey(Video, related_name='video_shares', on_delete=models.SET_NULL, null=True, blank=True)
+    source = models.ForeignKey(Video, related_name='video_shares', on_delete=models.SET(Video.objects.get(url='del')))
     url = models.CharField(max_length=256)
     thumb_nail = models.CharField(max_length=256, default='')
     author = models.ForeignKey(MyUser, related_name='my_shares')
