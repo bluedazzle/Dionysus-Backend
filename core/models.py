14a83ae467e32baf2cfbea0b0dd58b8f1dfef8dc
rapospectre <rapospectre@163.com>
2016-12-01 11:27:44 +0800

    <fix>  fix model
    Author:rapospectre@gmail.com
    
    Desc: fix model

--- core/models.py
+++ core/models.py
@@ -39,7 +39,7 @@ class Video(BaseModel):
     order = models.IntegerField(default=0)
 
     weibo_title = models.TextField(default=default_title)
-    wechat_title = models.TextField(default='')
+    wechat_title = models.TextField(default='', null=True)
     wechat_sub_title = models.TextField(default=default_title)
 
     def __unicode__(self):
