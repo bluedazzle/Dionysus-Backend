caa1a24d9291b40a22054c78b3d88b482c0411c6
rapospectre <rapospectre@163.com>
2017-01-16 17:24:14 +0800

    <feat>  add wechat share
    Author:rapospectre@gmail.com
    
    Desc: add wechat share

--- core/wechat_sign.py
+++ core/wechat_sign.py
@@ -6,15 +6,10 @@ import hashlib
 
 
 class Sign:
-    def __init__(self, appId, appSecret, url=None):
+    def __init__(self, appId, appSecret):
         self.appId = appId
         self.appSecret = appSecret
-
-        self.ret = {
-            'nonceStr': self.__create_nonce_str(),
-            'timestamp': self.__create_timestamp(),
-            'url': url
-        }
+        self.ret = {}
 
     def __create_nonce_str(self):
         return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(15))
@@ -25,6 +20,7 @@ class Sign:
     def sign(self, url, ticket):
         self.ret['url'] = url
         self.ret['timestamp'] = self.__create_timestamp()
+        self.ret['timestamp'] = self.__create_nonce_str()
         self.ret['jsapi_ticket'] = ticket
         string = '&'.join(['%s=%s' % (key.lower(), self.ret[key]) for key in sorted(self.ret)])
         self.ret['signature'] = hashlib.sha1(string).hexdigest()
